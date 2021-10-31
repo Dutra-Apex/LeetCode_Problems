92ebb94af5b8d3b32091a74b6933eaabafc09969
Gabriel Dutra <57206480+Dutra-Apex@users.noreply.github.com>
2021-10-31 19:49:50 -0400

    Create minPartitions.py

---
+++ minPartitions.py
@@ -0,0 +1,10 @@
+class Solution:
+    def minPartitions(self, n: str) -> int:
+        
+        arr = []
+        
+        for i in n:
+            arr.append(int(i))
+            
+        return max(arr)
+        
