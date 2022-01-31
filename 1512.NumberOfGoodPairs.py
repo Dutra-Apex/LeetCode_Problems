1c11065de7b8383b1d52f3580730442cbaa4bced
Gabriel Dutra <57206480+Dutra-Apex@users.noreply.github.com>
2022-01-30 23:58:12 -0500

    Create 1512.NumberOfGoodPairs.py

---
+++ 1512.NumberOfGoodPairs.py
@@ -0,0 +1,13 @@
+class Solution:
+    def numIdenticalPairs(self, nums: List[int]) -> int:
+        
+        counter = 0
+        
+        for i in range(len(nums)):
+            
+            for j in range(len(nums)):
+                
+                if  j > i and nums[i] == nums[j]:
+                    counter += 1
+                    
+        return counter
