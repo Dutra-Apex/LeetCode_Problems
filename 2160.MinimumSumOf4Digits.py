0067493fa9d9911b0023c942220f31a5a6b6d690
Gabriel Dutra <57206480+Dutra-Apex@users.noreply.github.com>
2022-02-15 23:54:14 -0500

    Update 2160.MinimumSumOf4Digits.py

--- 2160.MinimumSumOf4Digits.py
+++ 2160.MinimumSumOf4Digits.py
@@ -4,4 +4,5 @@ class Solution:
         arr = [int(i) for i in str(num)]
         arr = sorted(arr)
         
+       #Answer will always be the sum of the pairs between the smallest and largest numbers:
         return (int(str(arr[0]) + str(arr[2])) + int(str(arr[1]) + str(arr[3])))
