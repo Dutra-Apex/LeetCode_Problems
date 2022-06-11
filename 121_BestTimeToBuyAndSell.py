4f8dc1e546c508925ecb5b9832671b591fd055ca
Gabriel Dutra <57206480+Dutra-Apex@users.noreply.github.com>
2022-06-11 17:13:44 -0300

    Update 121_BestTimeToBuyAndSell.py

--- 121_BestTimeToBuyAndSell.py
+++ 121_BestTimeToBuyAndSell.py
@@ -1,11 +1,21 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
+          
+        if len(prices) < 2:
+            return 0
         
         profit = 0
-        
-        for i in range(len(prices)-1):
-            for j in range(i+1, len(prices)):
-                if prices[j] - prices[i] > profit:
-                    profit = prices[j] - prices[i]
-                    
+        left = 0
+        right = 1
+            
+        while right < len(prices):
+            
+            if prices[right] - prices[left] > profit:
+                profit = prices[right] - prices[left]
+            
+            if prices[left] > prices[right]:
+                left = right
+                
+            right += 1
+            
         return profit
