e390adebdae6a4e793840a4e60d4e610136f51d1
Gabriel Dutra <57206480+Dutra-Apex@users.noreply.github.com>
2022-06-16 23:21:50 -0300

    Create 41_FindingMissingPositive.py

---
+++ 41_FindingMissingPositive.py
@@ -0,0 +1,8 @@
+class Solution:
+    def firstMissingPositive(self, nums: List[int]) -> int:
+        
+        temp = set(nums)
+        
+        for i in range(1, len(nums)+2):
+            if i not in temp:
+                return i
