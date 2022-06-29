d8095e31dd57e2a8cf6cf47a112eda98a03e33bd
Gabriel Dutra <57206480+Dutra-Apex@users.noreply.github.com>
2022-06-28 23:46:27 -0300

    Update Height_Cracker.py

--- Height_Cracker.py
+++ Height_Cracker.py
@@ -1,11 +1,7 @@
 class Solution:
     def heightChecker(self, heights: List[int]) -> int:
-        
-        
-        sort_heights = sorted(heights)
-        
+        sort_heights = sorted(heights) 
         ans = 0
-        
         for i in range(len(heights)):
             if sort_heights[i] != heights[i]:
                 ans += 1
