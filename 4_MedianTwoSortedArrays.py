e6d24c5d53e79d2dcc841324299c79a70f8617c3
Gabriel Dutra <57206480+Dutra-Apex@users.noreply.github.com>
2022-08-02 23:47:40 -0300

    Update 4_MedianTwoSortedArrays.py

--- 4_MedianTwoSortedArrays.py
+++ 4_MedianTwoSortedArrays.py
@@ -2,7 +2,6 @@ class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
         
         nums3 = nums1 + nums2
-        
         nums3 = sorted(nums3)
         len_nums3 = len(nums3)
         median_index = len_nums3//2
