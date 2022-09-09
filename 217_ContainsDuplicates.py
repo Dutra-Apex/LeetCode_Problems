2f3430d0cc72db760ab9c90d01af518f26cddbc3
Gabriel Dutra <57206480+Dutra-Apex@users.noreply.github.com>
2022-09-08 19:59:02 -0700

    Create 217_ContainsDuplicates.py

---
+++ 217_ContainsDuplicates.py
@@ -0,0 +1,11 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        
+        nums = sorted(nums)
+        
+        for i in range(len(nums)-1):
+            
+            if nums[i+1] == nums[i]:
+                return True
+            
+        return False
